2cc90a1f7323d83381b7c8a8888df587c537b234
오윤 <oyun@oyun-ui-MacBookAir.local>
2022-11-27 21:44:39 +0900

    first commit

---
+++ config/urls.py
@@ -0,0 +1,27 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from covid import views
+from django.urls import path
+from covid import views
+
+urlpatterns = [
+    path('', views.home),
+    # '콤마 사이에 home을 넣으면 127.2ㅈ21~~~~/home'
+    path('admin/', admin.site.urls),
+]
